79e095042ee2175d784c184e475d2c38ab538633
Znamenskas <zvilandas@gmail.com>
2022-06-29 10:42:35 +0300

    Sukurtas naujas projektas,receptas

---
+++ sql_lenteliu_kurimas.sql
@@ -0,0 +1,18 @@
+INSERT INTO `darbuotojas`(
+    `id`,
+    `vardas`,
+    `pavardė`,
+    `gimimo_metai`,
+    `profesija`,
+    `stazas`,
+    `uzdarbis`
+)
+VALUES(
+    '11',
+    'Antanas',
+    'Antanaitis',
+    '1968',
+    'stalius',
+    '10',
+    '2000'
+)
